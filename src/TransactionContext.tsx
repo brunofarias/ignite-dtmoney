3b5fddab6fa2a059c59d8e11691d7c5d8bbcdad1
Bruno Farias <begfarias@gmail.com>
2021-03-21 19:03:20 -0300

    feat: Create context

---
+++ src/TransactionContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { api } from './services/api';
+
+interface Transaction {
+  id: number,
+  title: string,
+  type: string,
+  category: string,
+  amount: number,
+  createdAt: string
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode;
+}
+
+// transaction
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
+interface TransactionContextData {
+  transactions: Transaction[],
+  createTransaction: (transaction: TransactionInput) => Promise<void>;
+}
+
+export const TransactionContext = createContext<TransactionContextData>({} as TransactionContextData);
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => setTransactions(response.data.transactions))
+  }, []);
+
+  async function createTransaction(transactionInput: TransactionInput) {
+    const response = await api.post('/transactions', {
+      ...transactionInput,
+      createdAt: new Date()
+    });
+
+    const { transaction } = response.data;
+
+    setTransactions([
+      ...transactions,
+      transaction
+    ])
+
+  }
+
+  return (
+    <TransactionContext.Provider value={{ transactions, createTransaction }}>
+      {children}
+    </TransactionContext.Provider>
+  );
+}
